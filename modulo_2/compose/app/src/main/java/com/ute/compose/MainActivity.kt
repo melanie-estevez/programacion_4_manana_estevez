6e07db655ad73ac46a5374fa3915fdf480ef488d
Melanie Estévez <melaniestevez@hotmail.com>
2026-05-06 11:05:31 -0500

    clase 2026/05/06

--- modulo_2/compose/app/src/main/java/com/ute/compose/MainActivity.kt
+++ modulo_2/compose/app/src/main/java/com/ute/compose/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import com.ute.compose.ui.material3.*
 import com.ute.compose.ui.screens.*
 
 class MainActivity : ComponentActivity() {
@@ -12,10 +13,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
+                //Componentes basicos nativos:
                 //S01_SaludoScreen()
-                S02_TextScreen()
-                // ◀ CAMBIA AQUÍ para probar cada paso:
-                // Paso01_TextFieldScreen()
+                //S03_ButtonScreen()
+                //S04_LayoutScreen()
+                //S05_ModifierScreen()
+                //S06_EstadoScreen()
+                //S07_StateHoistingScreen()
+                //S08_BienvenidaScreen()
+                // Componentes Material 3:
+                Paso01_TextFieldScreen()
                 // Paso02_CardScreen()
                 // Paso03_LazyColumnScreen()
                 // Paso04_ScaffoldScreen()
