6e1d27aa167eae056dd21b3ed82c4d31fa956d86
Melanie Estevez <melaniestevez@hotmail.com>
2026-05-27 05:33:08 -0500

    clase 2026/05/27

--- modulo_2/ShopApplication/app/src/main/java/com/shopapp/ShopApplication.kt
+++ modulo_2/ShopApplication/app/src/main/java/com/shopapp/ShopApplication.kt
@@ -4,4 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ShopApp : Application()
+class ShopApplication : Application()
--- modulo_2/ShopApplication/app/src/main/java/com/shopapp/theme/Theme.kt
+++ modulo_2/ShopApplication/app/src/main/java/com/shopapp/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.shopapp.theme
 
-
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
