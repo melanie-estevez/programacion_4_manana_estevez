10d22c4b9e408e14781af36bb3fc3e2823375185
Melanie Estevez <melaniestevez@hotmail.com>
2026-06-10 05:58:42 -0500

    clase 10/06/2026

--- modulo_2/TechDash/app/src/main/java/com/ute/techdash/MainActivityRed.kt
+++ modulo_2/TechDash/app/src/main/java/com/ute/techdash/MainActivityRed.kt
@@ -10,22 +10,21 @@ import androidx.navigation.compose.rememberNavController
 import com.ute.techdash.ui.hardware.DashboardHardware
 import com.ute.techdash.ui.hardware.PantallaMenu
 import com.ute.techdash.ui.hardware.gps.PantallaGPS
+import com.ute.techdash.ui.hardware.red.BannerConectividad
+import com.ute.techdash.ui.hardware.red.ConectividadRepository
 import com.ute.techdash.ui.hardware.sensores.PantallaSensores
 import com.ute.techdash.ui.theme.TechDashTheme
 
-class MainActivity : ComponentActivity() {
+
+
+class MainActivityRed: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TechDashTheme {
-                val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "menu") {
-                    composable("menu")      { PantallaMenu(navController) }
-                    composable("gps")       { PantallaGPS() }
-                    composable("sensores")  { PantallaSensores() }
-                    composable("dashboard") { DashboardHardware() }
-                }
+                //BannerConectividad(),
+                //ConectividadRepository()
             }
         }
     }
