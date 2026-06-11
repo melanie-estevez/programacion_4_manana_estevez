0ddcf08add86e723e78141640ecc7aaa5abef7f4
Yosh-05 <joshuamena33@gmail.com>
2026-06-11 05:14:23 -0500

    clase 11-06-2026 check 1

--- modulo_2/ShopApplication/app/src/main/java/com/shopapp/domain/repository/UserRepository.kt
+++ modulo_2/ShopApplication/app/src/main/java/com/shopapp/domain/repository/UserRepository.kt
@@ -1,6 +1,8 @@
 package com.shopapp.domain.repository
+import android.net.Uri
 import com.shopapp.domain.model.User
 import com.shopapp.domain.model.UserPayload
+
 
 interface UserRepository {
     suspend fun getUsers(
@@ -15,4 +17,8 @@ interface UserRepository {
     suspend fun deleteUser(id: Int): Result<Unit>
     suspend fun toggleActive(id: Int): Result<Boolean>
     suspend fun getStats(): Result<Map<String, Int>>
+
+    suspend fun getProfile(): Result<User>
+
+    suspend fun uploadAvatar(uri: Uri): Result<String>
 }
