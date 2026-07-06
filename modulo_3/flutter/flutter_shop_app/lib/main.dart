7d22c97a5f31177a6e1477cdcf35226b824fe371
Melanie Estevez <melaniestevez@hotmail.com>
2026-07-06 10:17:20 -0500

    flutter_shop_app checkpoint 03-06-07-2026

--- modulo_3/flutter/flutter_shop_app/lib/main.dart
+++ modulo_3/flutter/flutter_shop_app/lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_shop_app/presentation/navigation/app_router.dart';
 import 'core/config/app_config.dart';
 import 'theme/app_theme.dart';
 import 'theme/app_colors.dart';
@@ -12,18 +13,22 @@ import 'domain/model/category.dart';
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
   runApp(const ProviderScope(child: FlutterShopApp()));
+  
 }
 
-class FlutterShopApp extends StatelessWidget {
+class FlutterShopApp extends ConsumerWidget {
   const FlutterShopApp({super.key});
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-    title:            AppConfig.appName,
-    debugShowCheckedModeBanner: false,
-    theme:            AppTheme.dark,
-    home:             const VerificationScreen(),
-  );
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    return MaterialApp.router(
+      title:                'Flutter Shop App',
+      debugShowCheckedModeBanner: false,
+      theme:                AppTheme.dark,
+      routerConfig:         router,
+    );
+  }
 }
 
 // Provider de verificación
