42c812445c1a04f6b6e51e345c4b7f7371f68dfe
Melanie Estevez <melaniestevez@hotmail.com>
2026-07-08 09:28:14 -0500

    flutter_shop_app checkpoint 08-ADMIN-CATEGORIES 08-07-2026

--- modulo_3/flutter/flutter_shop_app/lib/presentation/navigation/app_router.dart
+++ modulo_3/flutter/flutter_shop_app/lib/presentation/navigation/app_router.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_shop_app/presentation/screens/admin/categoriesadminscreen.dart';
 import 'package:flutter_shop_app/presentation/screens/admin/dashboard_screen.dart';
 import 'package:flutter_shop_app/presentation/screens/auth/profile_screen.dart';
 import 'package:flutter_shop_app/presentation/screens/cart/cart_screen.dart';
@@ -119,11 +120,11 @@ final routerProvider = Provider<GoRouter>((ref) {
         ),
       ),
       GoRoute(
-        path: '/admin/categories',
+        path:    '/admin/categories',
         builder: (_, state) => AdminShell(
           title:        'Categorías',
           currentRoute: state.matchedLocation,
-          child:        const _AdminPlaceholder('Categorías — M8'),
+          child:        const CategoriesAdminScreen(),
         ),
       ),
       GoRoute(
@@ -151,6 +152,7 @@ final routerProvider = Provider<GoRouter>((ref) {
               'Pedido #${state.pathParameters['id']} — M10'),
         ),
       ),
+      
       GoRoute(
         path: '/admin/users',
         builder: (_, state) => AdminShell(
